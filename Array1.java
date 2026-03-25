1fd47f3a4b36f9df7cb0287317a0900e512993c6
Sudarshan Sudhakar bhat <sudubhatjune23@gmail.com>
2026-03-25 21:09:30 +0530

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,22 @@
+
+import java.util.Arrays;
+
+public class Array1 {
+    public static void main(String[] args) {
+        int[] n=new int[5];
+        n[0]=10;
+         n[1]=20; 
+         n[2]=30;
+          n[3]=40;
+
+          System.out.println(Arrays.toString(n));
+          String []s={"sudu","vivek","tejas"};
+          for (int i = 0; i < s.length; i++){
+            System.out.println(s[i]);
+        
+          }
+
+          
+
+    }
+}
